51508a3ea7796eff44d35a7ee602ebcf0377cc97
tatidem <tdemyanova@mail.ru>
2023-12-10 12:36:34 +0400

    feat: add video

---
+++ coffee-house/js/api.js
@@ -0,0 +1,25 @@
+// Создаем новый объект XMLHttpRequest
+var xhr = new XMLHttpRequest();
+
+// Указываем тип запроса (GET) и адрес файла JSON
+xhr.open('GET', './products.json', true);
+
+// Устанавливаем заголовок Content-Type
+xhr.setRequestHeader('Content-Type', 'application/json');
+
+// Обработчик события, срабатывающий при изменении состояния запроса
+xhr.onreadystatechange = function () {
+  // Если запрос успешно завершен (readyState === 4) и статус ответа 200 (OK)
+  if (xhr.readyState === 4 && xhr.status === 200) {
+    // Распарсим полученные данные JSON
+    var products = JSON.parse(xhr.responseText);
+
+    // Теперь переменная products содержит объекты товаров из вашего JSON-файла
+
+    // Ваш код для обработки данных из products
+    console.log(products);
+  }
+};
+
+// Отправляем запрос
+xhr.send();
---
+++ coffee-house/js/main.js
@@ -0,0 +1,5 @@
+
+document.querySelector('.burger-icon').addEventListener('click', function() {
+    this.classList.toggle('active');
+    // Другие действия, которые вы хотите выполнить при клике
+  });
